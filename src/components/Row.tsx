1cc4c0af6dfa438debb5f3b095b20a32247d1e79
Nikolai Syrovezhko <syrovezhko@list.ru>
2023-09-29 19:48:31 +0300

    fix: removed borders

--- src/components/Row.tsx
+++ src/components/Row.tsx
@@ -11,12 +11,9 @@ const StyledRow = styled.div`
   align-items: flex-start;
   gap: 5rem;
   flex-shrink: 0;
-  border: 1px solid red;
   @media ${(props) => props.theme.media.mobile} {
-    border: 1px solid green;
   }
   @media ${(props) => props.theme.media.tablet} {
-    border: 1px solid yellow;
   }
 `;
 
