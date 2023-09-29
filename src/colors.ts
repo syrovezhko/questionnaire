e7fff1fef959e9889e8adb376f9befaf4b3bd424
Nikolai Syrovezhko <syrovezhko@list.ru>
2023-09-29 16:25:53 +0300

    feat: colors set

---
+++ src/colors.ts
@@ -0,0 +1,15 @@
+export const grayBlack         = '#1C1C1C';
+export const greyDark1         = '#0A0A0A';
+export const greyDark2         = '#0E0E0E';
+export const greyDark3         = '#161616';
+export const greyDark4         = '#2B2B2B';
+export const greyWhite         = '#FFF';
+export const greyLight         = '#CDCDCD';
+export const greyMedium        = '#5F5F5F';
+export const glassLight        = 'rgba(255, 255, 255, 0.05)';
+export const glassMedium       = 'rgba(28, 28, 28, 0.50)';
+export const glassDark         = 'rgba(16, 16, 16, 0.90)';
+export const inputGradient     = `linear-gradient(180deg, ${grayBlack} 0%, ${grayBlack} 100%)`;
+export const disabledNoHovered = 'rgba(255, 255, 255, 0.20)';
+export const success           = '#6DB95A';
+export const error             = '#DD5E5E';
