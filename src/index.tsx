30c19c4b49fdba6cffa92fe8be877d9add2770f1
Nikolai Syrovezhko <syrovezhko@list.ru>
2023-09-29 12:20:53 +0300

    feat: provider and store to index

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,12 @@
 import React from 'react';
 import { render } from 'react-dom';
 import App from './App';
+import { Provider } from 'react-redux';
+import { store } from './redux/questions';
 
-render(<App />, document.getElementById('root'));
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
