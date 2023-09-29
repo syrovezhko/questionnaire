864ef842de6bb82f3fbae50740ecfe16a99461c2
Nikolai Syrovezhko <syrovezhko@list.ru>
2023-09-29 18:57:17 +0300

    feat: `App~ styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const AppWrapper = styled.div`
+  width: 100%;
+  min-height: 100vh;
+  padding: 2rem;
+  background-color: ${(props) => props.theme.colors.grayBlack};
+  color: ${(props) => props.theme.colors.greyWhite};
+  display: flex;
+  justify-content: center;
+`;
 
 const App = () => {
-  return (
-      <h3>Hello World</h3>
-  );
+  return <AppWrapper>
+  </AppWrapper>;
 };
 
 export default App;
