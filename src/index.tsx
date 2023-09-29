ae3b3290f533613ab4424ca663751932c1c4bfa0
Nikolai Syrovezhko <syrovezhko@list.ru>
2023-09-29 18:58:01 +0300

    refactor: provide variables via ThemeProvider

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import { render } from 'react-dom';
 import App from './App';
 import { Provider } from 'react-redux';
 import { store } from './redux/questions';
-import { createGlobalStyle } from 'styled-components';
+import { ThemeProvider, createGlobalStyle } from 'styled-components';
 
 const Global = createGlobalStyle`
   * {
@@ -17,10 +17,43 @@ const Global = createGlobalStyle`
   }
 `;
 
+const sizes = {
+  desktop: '1280px',
+  tablet: '1024px',
+  mobile: '425px'
+};
+
+const theme = {
+  colors: {
+    grayBlack: '#1C1C1C',
+    greyDark1: '#0A0A0A',
+    greyDark2: '#0E0E0E',
+    greyDark3: '#161616',
+    greyDark4: '#2B2B2B',
+    greyWhite: '#FFF',
+    greyLight: '#CDCDCD',
+    greyMedium: '#5F5F5F',
+    glassLight: 'rgba(255, 255, 255, 0.05)',
+    glassMedium: 'rgba(28, 28, 28, 0.50)',
+    glassDark: 'rgba(16, 16, 16, 0.90)',
+    inputGradient: `linear-gradient(180deg, #1C1C1C 0%, #1C1C1C 100%)`,
+    disabledNoHovered: 'rgba(255, 255, 255, 0.20)',
+    success: '#6DB95A',
+    error: '#DD5E5E'
+  },
+  media: {
+    tablet: `(max-width: ${sizes.tablet}) and (min-width: ${sizes.mobile})`,
+    mobile: `(max-width: ${sizes.mobile})`
+  },
+  sizes: sizes
+};
+
 render(
   <Provider store={store}>
-    <Global />
-    <App />
+    <ThemeProvider theme={theme}>
+      <Global />
+      <App />
+    </ThemeProvider>
   </Provider>,
   document.getElementById('root')
 );
