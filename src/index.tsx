4092b78b479a17b0b959fa40207ee5117ded302b
Nikolai Syrovezhko <syrovezhko@list.ru>
2023-09-29 12:52:04 +0300

    feat: reset styles

--- src/index.tsx
+++ src/index.tsx
@@ -3,9 +3,23 @@ import { render } from 'react-dom';
 import App from './App';
 import { Provider } from 'react-redux';
 import { store } from './redux/questions';
+import { createGlobalStyle } from 'styled-components';
+
+const Global = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: 'Ubuntu';
+  }
+  html {
+    font-size: 10px;
+  }
+`;
 
 render(
   <Provider store={store}>
+    <Global />
     <App />
   </Provider>,
   document.getElementById('root')
