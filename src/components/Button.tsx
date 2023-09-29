658eb47067ee074b248ed9787fa4aafa323779ff
Nikolai Syrovezhko <syrovezhko@list.ru>
2023-09-29 21:37:36 +0300

    feat: tag type of `Button`

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -9,11 +9,11 @@ const Primary = styled.button`
   border: none;
   border-radius: 3rem;
   color: ${(props) => props.theme.colors.greyBlack};
-  /* color: ${(props) => props.color}; */
   display: flex;
   justify-content: center;
   align-items: center;
   cursor: pointer;
+  text-transform: uppercase;
   &:hover {
     text-decoration-line: line-through;
     text-decoration-thickness: 0.1rem;
@@ -25,17 +25,42 @@ const Secondary = styled(Primary)`
   color: ${(props) => props.theme.colors.greyWhite};
 `;
 
+const Tag = styled(Primary)`
+  border: 0.2rem solid ${(props) => props.theme.colors.greyDark2};
+  padding: 0.4rem 1.4rem;
+  border-radius: 2.5rem;
+  background-color: transparent;
+  color: ${(props) => props.theme.colors.greyWhite};
+  &:hover {
+    background-color: ${(props) => props.theme.colors.greyDark2};
+    text-decoration: none;
+  }
+  &:active {
+    color: ${(props) => props.theme.colors.grayBlack};
+    background-color: ${(props) => props.theme.colors.greyWhite};
+    border-color: ${(props) => props.theme.colors.greyWhite};
+  }
+`;
+
 const Button = (props) => {
-  if(props.type === 'secondary') return (
-    <Secondary>
-      <Text type={'button'} {...props} />
-    </Secondary>
-  )
-  else return (
-    <Primary>
-      <Text type={'button'} {...props} />
-    </Primary>
-  )
+  if (props.type === 'secondary')
+    return (
+      <Secondary>
+        <Text type={'button'} {...props} />
+      </Secondary>
+    );
+  else if (props.type === 'tag')
+    return (
+      <Tag>
+        <Text type={'label'} {...props} />
+      </Tag>
+    );
+  else
+    return (
+      <Primary>
+        <Text type={'button'} {...props} />
+      </Primary>
+    );
 };
 
 export default Button;
